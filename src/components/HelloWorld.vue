86ec292cd88145ad21fd1df67b0a29d074b9c993
georger <george.ravelly.111@ufrn.edu.br>
2023-01-20 13:35:00 -0300

    Page for generate passwords

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,14 +8,9 @@ defineProps({
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
-  </div>
+  <p class="me-8">
+    <h1>{{ msg }}</h1>
+  </p>
 </template>
 
 <style scoped>
--- src/components/MyInitialPage.vue
+++ src/components/MyInitialPage.vue
@@ -1,11 +1,84 @@
 <script>
+
 export default {
   props: ['title'],
-  setup() {},
+  data() {
+    return {
+      password: '',
+      options: {
+        number: false,
+        uppercase: false,
+        special: false
+      },
+      value: 4,
+      max: 24
+    }
+  },
+  methods: {
+    generatePassword() {
+      if (this.value < 4) return;
+      if (this.options.number) {
+        let min = Math.ceil(this.value);
+        let max = Math.floor(this.max);
+        this.password = Math.floor(Math.random() * (max - min) + min);
+        console.log(this.password);
+        return;
+      }
+      if (this.options.uppercase) {}
+    },
+    resetOptionUpperAndSpecial() {
+      this.options.special = false;
+      this.options.uppercase = false;
+    }
+  }
 }
 </script>
 
 <template>
-  <h1>{{ title }}</h1>
-  <v-btn color="primary">Teste</v-btn>
+  <v-row>
+    <v-col cols="9">
+      <v-text-field 
+        label="Password"
+        v-model="password">
+      </v-text-field>
+    </v-col>
+    <v-col cols="3">
+      <v-text-field 
+        v-model="value" 
+        type="number" 
+        step="1" 
+        min="4" 
+        max="24">
+      </v-text-field>
+    </v-col>
+  </v-row>
+  <div class="d-flex mb-5">
+    <input
+      type="checkbox"
+      label="Number"
+      v-model="options['number']"
+      @change="resetOptionUpperAndSpecial()"
+    >&nbsp; Only Numbers &nbsp; 
+    <input
+      type="checkbox"
+      label="Letters"
+      v-model="options['uppercase']"
+      :disabled="options.number"
+    >
+    <span :disabled="options.number">&nbsp; Uppercase &nbsp;</span> 
+    <input
+      type="checkbox"
+      label="Caracter especial"
+      v-model="options['special']"
+      :disabled="options.number"
+    >
+    <span :disabled="options.number">&nbsp; Special Char</span>
+  </div>
+  <v-btn
+    color="primary"
+    @click="generatePassword()"
+    >Generate</v-btn>
 </template>
+
+<style scoped>
+</style>

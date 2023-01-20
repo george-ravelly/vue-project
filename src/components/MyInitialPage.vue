bf2196bcc325b1e4395815e2556a957ff09434e4
georger <george.ravelly.111@ufrn.edu.br>
2023-01-20 11:02:30 -0300

    Config vuetify

---
+++ src/components/MyInitialPage.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  props: ['title'],
+  setup() {},
+}
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <v-btn color="primary">Teste</v-btn>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+// import TheWelcome from '../components/TheWelcome.vue'
+import MyInitialPage from '../components/MyInitialPage.vue'
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <MyInitialPage title="Olá mundo"/>
+  </main>
+</template>
